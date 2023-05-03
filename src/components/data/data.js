1a79f64fca79c2f60579353ffce9f2870a6e6fac
Letícia Chaves <leticiaschaves7@gmail.com>
2023-05-02 21:18:35 -0300

    degradê mais bonito

--- src/components/data/data.js
+++ src/components/data/data.js
@@ -1,12 +1,38 @@
-const data = [
+export const data = [
     {
-        "name": "manjuba",
-        "id": "1",
-        "about": "Na Manjuba, acreditamos que a tecnologia deve ser acessível, transparente e ética. Somos uma empresa de desenvolvimento de software comprometida em resolver problemas digitais de maneira responsável e criativa. Estamos aqui para criar soluções que melhoram a vida das pessoas e das empresas.",
-        "mission": "Nossa missão é democratizar o acesso à tecnologia, simplificando processos e entregando soluções personalizadas para cada cliente. Nós nos esforçamos para construir relações duradouras, baseadas na confiança, transparência e colaboração.",
-        "vision": "Nosso objetivo é ser uma referência em desenvolvimento de software ético e responsável, reconhecidos pela qualidade de nossas soluções e pela excelência de nosso atendimento. Na Manjuba, estamos comprometidos em construir um futuro melhor para todos, e estamos prontos para enfrentar os desafios do presente e do futuro.",
-        "values": "Acreditamos que a inovação é essencial para alcançar nossos objetivos, mas também valorizamos a tradição e a história da nossa região. Buscamos preservar o patrimônio natural e cultural do Piauí, trabalhando em harmonia com a comunidade e apoiando iniciativas de desenvolvimento sustentável."
+        id: 1,
+        category: 'about',
+        title: 'Manjuba',
+        content: 'Na Manjuba, acreditamos que a tecnologia deve ser acessível, transparente e ética. Somos uma empresa de desenvolvimento de software comprometida em resolver problemas digitais de maneira responsável e criativa. Estamos aqui para criar soluções que melhoram a vida das pessoas e das empresas.'
+    },
+    {
+        id: 2,
+        category: 'values',
+        title: 'Valores',
+        content: 'Acreditamos que a inovação é essencial para alcançar nossos objetivos, mas também valorizamos a tradição e a história da nossa região. Buscamos preservar o patrimônio natural e cultural do Piauí, trabalhando em harmonia com a comunidade e apoiando iniciativas de desenvolvimento sustentável.'
+    },
+    {
+        id: 3,
+        category: 'mission',
+        title: 'Missão',
+        content: 'Nossa missão é democratizar o acesso à tecnologia, simplificando processos e entregando soluções personalizadas para cada cliente. Nós nos esforçamos para construir relações duradouras, baseadas na confiança, transparência e colaboração.'
+    },
+    {
+        id: 4,
+        category: 'vision',
+        title: 'Objetivo',
+        content: 'Nosso objetivo é ser uma referência em desenvolvimento de software ético e responsável, reconhecidos pela qualidade de nossas soluções e pela excelência de nosso atendimento. Na Manjuba, estamos comprometidos em construir um futuro melhor para todos, e estamos prontos para enfrentar os desafios do presente e do futuro.'
     }
 ]
 
-export default data;
+export const categories = [
+    {
+        name: 'mission',
+    },
+    {
+        name: 'vision'
+    },
+    {
+        name: 'values',
+    },
+];
--- src/components/main/AboutContent.jsx
+++ src/components/main/AboutContent.jsx
@@ -1,44 +1,28 @@
 import React from 'react';
 import './AboutContent.css';
-import data from '../data/data';
+import { data } from '../data/data';
 
-function AboutContent() {
- const {
-    about,
-    mission,
-    vision,
-    values,
-  } = data[0];
-  
+function AboutContent() {  
   return (
     <section className="about" id="about">
-      <article className="resume">
-        <h3>Quem somos</h3>
-        <p>{about}</p>
-      </article>
-      <article className="resume">
-        <h3
-          type="valores">
-            Valores
-        </h3>
-        <p>{values}</p>
-      </article>
-      <article className="resume">
-        <h3
-          type="mission"
-          className="mision">
-            Missão
-        </h3>
-        <p>{mission}</p>
-      </article>
-      <article className="resume">
-        <h3
-          type="vision"
-          className="vision">
-            Visão
-        </h3>
-        <p>{vision}</p>
-      </article>
+      <h3>
+        Sobre nós
+      </h3>
+      {
+        data.map((e) => (
+          <article
+          className="resume"
+          key={e.id}>
+            <h2
+            type="button">
+              {e.title}
+            </h2>
+            <p>
+              {e.content}
+              </p>
+          </article>
+        ))
+      }
     </section>
   );
 }
